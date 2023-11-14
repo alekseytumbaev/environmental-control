2eb14a1bf15194068ff4dd3d5b0e503a3dea78fe
Aleksey Tumbaev <leha.tumbaev@gmail.com>
2023-11-14 09:12:59 +0700

    initial commit

---
+++ src/main/java/com/example/environmentalcontrol/EnvironmentalControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.environmentalcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentalControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnvironmentalControlApplication.class, args);
+    }
+
+}
